26cb74fe49dc3a96fa7629e6ea3c9a8afec05031
Trong Le <t.q.le.email@gmail.com>
2023-08-27 19:50:57 -0700

    Solve Challenge 7 Step 1 & 2

--- index.js
+++ index.js
@@ -168,7 +168,12 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
-
+const [firstGame, secondGame, ...remainingGames] = sortedGames;
 // create a new element to hold the name of the top pledge game, then append it to the correct element
-
+const firstGameNameElement = document.createElement("p");
+firstGameNameElement.innerHTML = firstGame.name;
+firstGameContainer.appendChild(firstGameNameElement);
 // do the same for the runner up item
+const secondGameNameElement = document.createElement("p");
+secondGameNameElement.innerHTML = secondGame.name;
+secondGameContainer.appendChild(secondGameNameElement);
