cb59ba906fc51b4e92232fc69c6bcf6af212dc32
mnhk0015 <87726264+MohammedASC20@users.noreply.github.com>
2024-01-02 03:48:57 -0500

    Challenge 4 Completed. Added the Contributions and Raised Card.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# WEB102 Prework - *Name of App Here*
+# WEB102 Prework - *SMC GAME HUB*
 
-Submitted by: **Your Name Here**
+Submitted by: **Mohammed Nazib Hossain Khan**
 
-**Name of your app** is a website for the company Sea Monster Crowdfunding that displays information about the games they have funded.
+**SMC GAME HUB** is a website for the company Sea Monster Crowdfunding that displays information about the games they have funded.
 
 Time spent: **X** hours spent in total
 
@@ -38,7 +38,7 @@ Describe any challenges encountered while building the app.
 
 ## License
 
-    Copyright [yyyy] [name of copyright owner]
+    Copyright [2023] [name of copyright owner]
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
--- index.js
+++ index.js
@@ -68,16 +68,24 @@ addGamesToPage(GAMES_JSON);
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
-
+const totalContributions = GAMES_JSON.reduce((total,cbts) => {
+    return total+cbts.backers
+},0) 
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
 
+contributionsCard.innerHTML = `<h3>Total Contributors:${totalContributions.toLocaleString()}</h3>`;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
 
+const totalRaised = GAMES_JSON.reduce((total, raised) => {
+    return total+raised.pledged
+},0)
+
 // set inner HTML using template literal
 
+raisedCard.innerHTML = `<h3>Total Raised: ${totalRaised.toLocaleString()} </h3>`;
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
