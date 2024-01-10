ec6ecc51714497714937f10a6c8835e60d5eb358
DavidEValdes <davideduardovaldes@gmail.com>
2024-01-09 23:08:04 -0500

    challenge 5 commit

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function addGamesToPage(games) {
 
         // append the game to the games-container
         gamesContainer.appendChild(gameCard);
-    }
+    } 
 }
 // call the function we just defined using the correct variable
 // later, we'll call this function using a different list of games
@@ -69,19 +69,24 @@ const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
 
+const totalContributions = GAMES_JSON.reduce((acc, game) => acc + game.backers, 0);
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
 
+contributionsCard.innerHTML = `Total Contributions: ${totalContributions.toLocaleString('en-US')}`;
 
 // grab the amount raised card, then use reduce() to find the total amount raised
+
 const raisedCard = document.getElementById("total-raised");
+const totalRaised = GAMES_JSON.reduce((acc, game) => acc + game.pledged, 0);
 
 // set inner HTML using template literal
 
+raisedCard.innerHTML = `Total Raised: $${totalRaised.toLocaleString('en-US')}`;
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
-
+gamesCard.innerHTML = `Number of Games: ${GAMES_JSON.length}`;
 
 /*************************************************************************************
  * Challenge 5: Add functions to filter the funded and unfunded games
