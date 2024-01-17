e0da94cb4a45571d13a25054d3c87c34add3ef55
shifterbit <48891590+shifterbit@users.noreply.github.com>
2024-01-16 20:55:06 -0500

    generate stats from games list

--- index.js
+++ index.js
@@ -71,19 +71,32 @@ addGamesToPage(GAMES_JSON)
 const contributionsCard = document.getElementById("num-contributions");
 
 // use reduce() to count the number of total contributions by summing the backers
+const totalContributions = GAMES_JSON.reduce(((acc, game) => {
+    return acc + game.backers
+}),0)
 
 
 // set the inner HTML using a template literal and toLocaleString to get a number with commas
+contributionsCard.innerHTML = ` ${totalContributions.toLocaleString('en-US')} `
 
 
 // grab the amount raised card, then use reduce() to find the total amount raised
 const raisedCard = document.getElementById("total-raised");
 
+const totalRaised = GAMES_JSON.reduce(((acc, game) => {
+    return acc + game.pledged
+}),0)
+
 // set inner HTML using template literal
+raisedCard.innerHTML = `$${totalRaised.toLocaleString('en-US')}`
 
 
 // grab number of games card and set its inner HTML
 const gamesCard = document.getElementById("num-games");
+
+const totalGames = GAMES_JSON.length
+
+gamesCard.innerHTML = `${totalGames.toLocaleString('en-US')}`
 
 
 /*************************************************************************************
