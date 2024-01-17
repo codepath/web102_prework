81604f22092f73c030fc99c38b7ae6a4886e0929
shifterbit <48891590+shifterbit@users.noreply.github.com>
2024-01-16 22:02:11 -0500

    display a message informing the user about which games may be underfunded

--- index.js
+++ index.js
@@ -159,12 +159,17 @@ allBtn.addEventListener("click", showAllGames)
 const descriptionContainer = document.getElementById("description-container");
 
 // use filter or reduce to count the number of unfunded games
+const numberOfUnfundedGames = GAMES_JSON.filter((game) => {return game.pledged < game.goal}).length
 
 
 // create a string that explains the number of unfunded games using the ternary operator
+const displayStr = `A total of $${totalRaised.toLocaleString('en-US')} has been raised for ${(totalGames > 1) ? `${totalGames} games`: `${totalGames} game`}. Currently  ${(numberOfUnfundedGames > 1) ? `${numberOfUnfundedGames} games remain`: `${numberOfUnfundedGames} game remains`} unfunded. We need your help to fund these amazing games!`
 
 
 // create a new DOM element containing the template string and append it to the description container
+const unfundedNotice = document.createElement(`p`)
+unfundedNotice.innerHTML = displayStr
+descriptionContainer.appendChild(unfundedNotice)
 
 /************************************************************************************
  * Challenge 7: Select & display the top 2 games
